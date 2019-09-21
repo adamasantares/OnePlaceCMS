89075e75afce9526ef4f5767c1f07ebdd2b519b8
Vladislav <irresistible1997@gmail.com>
2019-09-21 18:57:20 +0600

    new query

--- app/Entry.php
+++ app/Entry.php
@@ -12,6 +12,7 @@ class Entry extends Eloquent implements HasMedia
     use HasMediaTrait;
 
     protected $fillable = ['title', 'published', 'model_id', 'api_id', 'fields'];
+    protected $appends = ['formatted_fields'];
 
     public function registerMediaConversions(Media $media = null)
     {
@@ -29,5 +30,45 @@ class Entry extends Eloquent implements HasMedia
     public function setPublishedAttribute($value)
     {
         $this->attributes['published'] = ($value === 'true') || ($value === true);
+    }
+
+    public function getFormattedFieldsAttribute()
+    {
+        $result = [];
+        $modelFields = $this->model->fields;
+
+        foreach ($modelFields as $field) {
+            $key = $field['api_id'];
+
+            switch ($field['type']) {
+                case "media":
+                    $medias = $this->getMedia($key);
+
+                    foreach ($medias as $media) {
+                        $result[$key][] = $media->getFullUrl();
+                    }
+
+                    break;
+
+                case "relation":
+                    if($field['validations']['relation_type'] === 'one_to_many') {
+                        if(isset($rootValue->fields[$key])) {
+                            $result[$key] = Entry::whereIn('_id', $this->fields[$key])->where('published', true)->get();
+                        } else {
+                            $result[$key] = null;
+                        }
+                    }
+
+                    break;
+
+                default:
+                    $result[$key] = $this->fields[$key];
+
+                    break;
+            }
+
+        }
+
+        return $result;
     }
 }
